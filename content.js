61306001906276270a87584c6eec602c1668c6bb
Matthew Cranham <cranman@hotmail.co.uk>
2011-11-21 14:56:31 +0000

    Sorting out relative urls

--- content.js
+++ content.js
@@ -2,8 +2,19 @@ $('a').click(function (b) {
 	if (b.which == 2) { 	// If middle click detected
 		var tab = new Object();
 		tab.url = $(this).attr("href");
-
-		chrome.extension.sendRequest(tab);
+		
+		chrome.extension.sendRequest(parseUrl(tab));
 		return false;		// Stop current page from also loading link
 	}
 });
+
+function parseUrl(tab) {
+	var isRelativeUrl = /^\/.*/;	// Relative URL begins with a / and not a protocol
+	if (isRelativeUrl.test(tab.url)) {
+		// Construct complete URL from data about current URL
+		// Probably a better way of doing this
+		tab.url = window.location.protocol + "//" + window.location.hostname + tab.url
+	}
+	
+	return tab;
+}
