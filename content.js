057851232792edb80932584920346903597b6155
Matthew Cranham <cranman@hotmail.co.uk>
2011-11-21 21:14:18 +0000

    should have fixed relative URLs

--- content.js
+++ content.js
@@ -2,19 +2,29 @@ $('a').click(function (b) {
 	if (b.which == 2) { 	// If middle click detected
 		var tab = new Object();
 		tab.url = $(this).attr("href");
-		
+
 		chrome.extension.sendRequest(parseUrl(tab));
 		return false;		// Stop current page from also loading link
 	}
 });
 
 function parseUrl(tab) {
-	var isRelativeUrl = /^\/.*/;	// Relative URL begins with a / and not a protocol
-	if (isRelativeUrl.test(tab.url)) {
-		// Construct complete URL from data about current URL
-		// Probably a better way of doing this
-		tab.url = window.location.protocol + "//" + window.location.hostname + tab.url
+	var isAbsoluteUrl = /^\w+:\/\//
+
+	if (!(isAbsoluteUrl.test(tab.url))) {
+		if (/^[^.]/.test(tab.url)) {
+			// If starts with / or [^.] then append tohostname
+			tab.url = window.location.protocol + "//" + window.location.hostname + tab.url;
+		} else if (/.*\/$/.test(window.location.href)) {
+			// else if starts with [.] then if ends in / append
+			tab.url = window.location.href + "/" + tab.url;
+		} else {
+			// else lose last bit until / and append
+			var str = window.location.href.split("/");
+			str.pop();
+			tab.url = str.join("/") + "/" + tab.url;
+		}
 	}
-	
+
 	return tab;
 }
